c3a841cd6a0d42e9d68644dacb25be5b9698a364
rockypzhang <rockypzhang@tencent.com>
2022-10-21 15:22:15 +0800

    Revert "update"
    This reverts commit 709aa29396b223174883c0c97d3ccf35fdf8dab6.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,18 +6,14 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:sentry_flutter/sentry_flutter_web.dart';
-import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  FluttertoastWebPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   SentryFlutterWeb.registerWith(registrar);
-  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
